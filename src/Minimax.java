173cee64b2e64c1a784ca03ae12756b6a0bafd7b
mivan50 <s4014458@student.rmit.edu.au>
2024-02-10 17:43:01 +1100

    Implemented alpha-beta pruning, computer can now comfortably look 11 moves in advance

--- src/Minimax.java
+++ src/Minimax.java
@@ -2,22 +2,18 @@ public class Minimax {
     public static int[] findBestMove(char[][] board) {
         int bestVal = Integer.MIN_VALUE;
         int[] bestMove = {-1, -1};
-        int alpha = Integer.MIN_VALUE;
-        int beta = Integer.MAX_VALUE;
         int[] columnOrder = {3, 2, 4, 1, 5, 0, 6};
 
         for (int i : columnOrder) {
-            for (int j = 5; j >= 0; j--) {
-                if (board[j][i] == ' ') {
-                    board[j][i] = 'R';
-                    int moveVal = minimax(board, 0, false);
-                    board[j][i] = ' ';
-                    if (moveVal > bestVal) {
-                        bestMove[0] = j;
-                        bestMove[1] = i;
-                        bestVal = moveVal;
-                    }
-                    break;
+            int j = getEmptySpace(board, i);
+            if (j != -1) {
+                board[j][i] = 'R';
+                int moveVal = minimax(board, 0, Integer.MIN_VALUE, Integer.MAX_VALUE, false);
+                board[j][i] = ' ';
+                if (moveVal > bestVal) {
+                    bestMove[0] = j;
+                    bestMove[1] = i;
+                    bestVal = moveVal;
                 }
             }
         }
@@ -25,7 +21,7 @@ public class Minimax {
         return bestMove;
     }
 
-    public static int minimax(char[][] board, int depth, boolean isMaximising) {
+    public static int minimax(char[][] board, int depth, int alpha, int beta, boolean isMaximising) {
         char winner = Board.checkWinner(board);
         if (winner != ' ') {
             if (winner == 'Y') {
@@ -34,7 +30,7 @@ public class Minimax {
                 return 43 - depth;
             }
         }
-        if (Board.isBoardFull(board) || depth >= 7) {
+        if (Board.isBoardFull(board) || depth >= 11) {
             return 0;
         }
 
@@ -43,31 +39,42 @@ public class Minimax {
         if (isMaximising) {
             int bestScore = Integer.MIN_VALUE;
             for (int i : columnOrder) {
-                for (int j = 5; j >= 0; j--) {
-                    if (board[j][i] == ' ') {
-                        board[j][i] = 'R';
-                        int score = minimax(board, depth + 1, false);
-                        board[j][i] = ' ';
-                        bestScore = Math.max(bestScore, score);
-                        break;
-                    }
+                int j = getEmptySpace(board, i);
+                if (j != -1) {
+                    board[j][i] = 'R';
+                    int score = minimax(board, depth + 1, alpha, beta, false);
+                    board[j][i] = ' ';
+                    bestScore = Math.max(bestScore, score);
+                    alpha = Math.max(alpha, bestScore);
+                    if (beta <= alpha)
+                        break; // Beta cut-off
                 }
             }
             return bestScore;
         } else {
             int bestScore = Integer.MAX_VALUE;
             for (int i : columnOrder) {
-                for (int j = 5; j >= 0; j--) {
-                    if (board[j][i] == ' ') {
-                        board[j][i] = 'Y';
-                        int score = minimax(board, depth + 1, true);
-                        board[j][i] = ' ';
-                        bestScore = Math.min(bestScore, score);
-                        break;
-                    }
+                int j = getEmptySpace(board, i);
+                if (j != -1) {
+                    board[j][i] = 'Y';
+                    int score = minimax(board, depth + 1, alpha, beta, true);
+                    board[j][i] = ' ';
+                    bestScore = Math.min(bestScore, score);
+                    beta = Math.min(beta, bestScore);
+                    if (beta <= alpha)
+                        break; // Alpha cut-off
                 }
             }
             return bestScore;
         }
+    }
+
+    private static int getEmptySpace(char[][] board, int col) {
+        for (int i = 5; i >= 0; --i) {
+            if (board[i][col] == ' ') {
+                return i;
+            }
+        }
+        return -1;
     }
 }
